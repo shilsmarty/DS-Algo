ebccc80c4149049fded2a8380f5dfa985602ad80
shilsmarty <shiladitya.srivastava@gmail.com>
2017-07-08 02:28:10 -0700

    Adding BinaryTree class.

---
+++ MyLearnings/DataStructure/Generics/BinaryTree/BinaryTree.cs
@@ -0,0 +1,28 @@
+﻿
+namespace Learnings.DS.Algo.DataStructure.Generics.BinaryTree
+{
+    /// <summary>
+    /// Binart Tree implementation.
+    /// </summary>
+    /// <typeparam name="T"></typeparam>
+    public class BinaryTree<T>
+    {
+        private BinaryTreeNode<T> root = null;
+
+        public BinaryTree()
+        {
+            root = null;
+        }
+        public void Clear()
+        {
+            root = null;
+        }
+
+        public BinaryTreeNode<T> Root
+        {
+            get { return root; }
+            set { root = value; }
+        }
+
+    }
+}
---
+++ MyLearnings/DataStructure/Generics/BinaryTree/BinaryTreeDemo.cs
@@ -0,0 +1,22 @@
+﻿using Learnings.DS.Algo.DataStructure.Generics.BinaryTree;
+
+namespace Learnings.DS.Algo.DataStructure.Demo
+{
+    class BinaryTreeDemo
+    {
+        private static BinaryTree<int> btree = new BinaryTree<int>();
+
+        public static void CompleteBinaryTree()
+        {
+            btree.Root = new BinaryTreeNode<int>(1);
+            btree.Root.Left = new BinaryTreeNode<int>(2);
+            btree.Root.Right = new BinaryTreeNode<int>(3);
+            btree.Root.Left.Left = new BinaryTreeNode<int>(4);
+            btree.Root.Right.Right = new BinaryTreeNode<int>(5);
+            btree.Root.Left.Left.Right = new BinaryTreeNode<int>(6);
+            btree.Root.Right.Right.Right = new BinaryTreeNode<int>(7);
+            btree.Root.Right.Right.Right.Right = new BinaryTreeNode<int>(8);
+        }
+    }
+}
+
